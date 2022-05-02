fddb8c46c0b30e8a2bc28ed30415c85fb4d0de37
daram <dmdgi37@naver.com>
2022-05-03 00:06:14 +0900

    count 업데이트, delete 업데이트

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,28 +1,19 @@
 import React, { Component } from 'react';
 
+
 class Habit extends Component {
-    //state : 컴포넌트에 있는 상태 값이다. 객체의 형태들
-    state = {
-        
-        count: 0, //안에는 숫자를 올리고 내릴 수 있는 count가 들어있음
-    };
-    
+
     //render()의 return 에서 써주기 위해서 handle을 만든다. 이건 증가할 수 있도록 한 handle 컨트롤러이다.
     handleIncrement = () => {
-   
-
-        //state 오브젝트 안에 있는 count를 증가한 뒤 state를 업데이트 해야 한다.
-        this.setState({count: this.state.count + 1});
-        //리엑트에서 업데이트 할 때는 setState라는 함수를 호출해주어야 업데이트 되었는지 안다.
+        this.props.OnIncrement(this.props.habitsss);
     };
     //감소를 위한 handle 컨트롤러이다.
     handleDecrement = () => {
-        //state 오브젝트 안에 있는 count를 감소한 뒤 state를 업데이트 해야 한다.
-        const count = this.state.count - 1; 
-        this.setState({count: count < 0 ? 0 : count});
-        //리엑트에서 업데이트 할 때는 setState라는 함수를 호출해주어야 업데이트 되었는지 안다.
-        //감소이니까 -1이 되면 안되므로 count 변수를 만들어 준 다음에 0이라면 0 아니라면 count를 출력할 수 있는 간단한 조건문을 만든다.
+        this.props.OnDecrement(this.props.habitsss);
     };
+    handleDelete = () => {
+        this.props.OnDelete(this.props.habitsss);
+    }
     render() {
         //우리가 props로 받아 온 habitsss의 object인 name과 count 를 받아온다.
         //habits로 가보면 props를 Habit= habitsss{habitssss} 로 해주었음 여기서 habitssss는 object들임 그냥 function(habitssss) 같이 map으로 하나하나 탐색한것이다.
@@ -38,7 +29,7 @@ class Habit extends Component {
             <button className="habit-button habit-decrease" onClick={this.handleDecrement}>
                 <i className="fas fa-minus-square"></i>
             </button>
-            <button className="habit-button habit-delete">
+            <button className="habit-button habit-delete" onClick={this.handleDelete}>
                 <i className="fas fa-trash"></i>
             </button>
         </li> 
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -4,21 +4,54 @@ import Habit from './habit';
 class Habits extends Component {
     //habits 는 habit 들을 나열한 것이다. 다양한 habit들을 넣을 수 있게 잘라놓은 도큐먼트이다.
     state = {
+    //state에는 id, name, count 가 들어있는데 여기서 이 id는 list key 이므로
+    //리액트에서는 key값을 기준으로 자체판별해서 업데이트 할건지 안할건지를 구분하는데
+    //key값이 이미 업데이트 되어있는 경우, key값을 기준으로 내용이 바뀌지 않았다면 업데이트가 안되게끔 해준다.
+    //그렇기에 list key를 주어서 key 값을 만들어줌 (단 배열의 index는 안됨 index 안에 있는 값이 바뀔수도 있음) key는 바뀌면 안되는 고유한 값임
+   
         habits:[
             {id: 1, name: 'Reading', count : 0},
             {id: 2, name: 'Running', count : 0},
             {id: 3, name: 'Coding', count : 0},
         ],
-    }
-    //state에는 id, name, count 가 들어있는데 여기서 이 id는 list key 이므로
-    //리액트에서는 key값을 기준으로 자체판별해서 업데이트 할건지 안할건지를 구분하는데
-    //key값이 이미 업데이트 되어있는 경우, key값을 기준으로 내용이 바뀌지 않았다면 업데이트가 안되게끔 해준다.
-    //그렇기에 list key를 주어서 key 값을 만들어줌 (단 배열의 index는 안됨 index 안에 있는 값이 바뀔수도 있음) key는 바뀌면 안되는 고유한 값임
+       
+    };
+   
+    //데이터를 가지고 있는 곳이 내가 이 데이터를 어떻게 수정하면 되는 지 잘 알고있는 최고의 장소
+    //여기에서 count나 어떤 것을 수정해주는 것이 가장 좋다.
+    handleIncrement = (habit) => {
+    //어떤 특정한 습관을 인자로 전달받으면 그 습관에 해당하는 count를 증가해주면 됨
+    //리액트에서는 state를 직접적으로 수정하면 안된다
+    const habitssss = [...this.state.habits];
+    //...이연산자는 https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Operators/Spread_syntax 배열 안에 있는 아이템들을 하나하나씩 새로운 배열 안으로 복사해오는 것임
+    const index = habitssss.indexOf(habit);
+    habitssss[index].count++;
+    this.setState({ habits: habitssss});
+    
+    };
+    
+    handleDecrement = (habit) => {
+        const habitssss = [...this.state.habits];
+        //...이연산자는 https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Operators/Spread_syntax 배열 안에 있는 아이템들을 하나하나씩 새로운 배열 안으로 복사해오는 것임
+        const index = habitssss.indexOf(habit);
+        const count = habitssss[index].count - 1;
+        habitssss[index].count = count < 0 ? 0 : count;
+        this.setState({habits: habitssss});
+    };
+    handleDelete = (habit) => {
+        const habits = this.state.habits.filter(item => item.id !== habit.id);
+        this.setState({habits: habits})
+    };
+   
     render() {
         return (
             <ul>
                 {this.state.habits.map(habitssss => (
-                    <Habit key={habitssss.id} habitsss={habitssss}/>
+                    <Habit key={habitssss.id} habitsss={habitssss} 
+                    OnIncrement={this.handleIncrement}
+                    OnDecrement={this.handleDecrement} 
+                    OnDelete={this.handleDelete}
+                    />
                 ))}
             </ul>
             //이렇게 habits에서 state에 만들어 놓았던 habits들을 map으로 전체적으로 탐색해서 
