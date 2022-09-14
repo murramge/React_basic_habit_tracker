9fb5082634c3bda0f8b601c76253b7b9c368c454
daram <dmdgi37@naver.com>
2022-09-14 16:48:47 +0900

    deployment

--- src/components/simpleHabit.jsx
+++ src/components/simpleHabit.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { useCallback } from "react";
+import { useEffect } from "react";
 import { useState } from "react";
 const SimpleHabit = (props) => {
   const [count, setCount] = useState(0);
 
-  const handleIncrement = () => {
+  const handleIncrement = useCallback(() => {
     setCount(count + 1);
-  };
+  });
+
+  useEffect(() => {
+    console.log(`ddddddd`);
+  }, []);
   return (
     <li className="habit">
       <span className="habit-name">Reading</span>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./app";
-import "@fortawesome/fontawesome-free/js/all.js";
-import SimpleHabit from "./components/simpleHabit";
 
+import "@fortawesome/fontawesome-free/js/all.js";
+
+import App from "./app";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <SimpleHabit />
+    <App />
   </React.StrictMode>
 );
